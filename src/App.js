56750239bb013c829ef08e09804c6654cceb9bdc
JoLeaper <joleap123@gmail.com>
2020-04-29 14:20:19 -0700

    added basic structure, CSS styling, state, and starting search terms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './Header.js'
+import SearchResults from './SearchResults.js'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    pokemonName: '',
+    pokemonType: '',
+    PokemonAttack: 0,
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Header />
+        <div className='search'>
+          <div className='searchbox'>
+            <input>
+            </input>
+              <select option>
+                <option value='asc'>Ascending</option>
+                <option value='desc'>Descending</option>
+              </select>
+        </div>
+          <SearchResults />
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/SearchResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './SearchResults.css'
+
+export default class SearchResults extends Component {
+    render() {
+        return (
+            <div className='searchresults'>
+            </div>
+        )
+    }
+}
